c6516b3e76b3ecd7abc2a1b1a56784995c93f09c
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-12 02:27:05 +0800

    exact continue

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,10 +1,27 @@
-export default function Product() {
+export default function Product({ items, longText }) {
+  if (longText) {
+    return (
+      <div className="flex mx-2 my-4 col-span-2">
+        <div className="h-20 p-4 bg-[#ffffff]/80 rounded-2xl mr-3">
+          {" "}
+          <img className="h-14 w-16" src={longText?.url} alt="bg" />
+        </div>
+        <div className=" font-medium">
+          <p className="text-base font-bold ">{longText?.title}</p>
+          <p className="text-xs">{longText?.desc}</p>
+        </div>
+      </div>
+    );
+  }
   return (
-    <div className="flex">
-      <img className="h-16" src="/assets/images/billiard-photo/a1.svg" />
-      <div className="ml-3">
-        <p>Стандарт 6 ширээ</p>
-        <p>Заалны ширээ</p>
+    <div className="flex mx-2 my-4 col-span-2">
+      <div className="h-20 p-4 bg-[#ffffff]/80 rounded-2xl">
+        {" "}
+        <img className="h-14 w-14" src={items?.url} alt="bg" />
+      </div>
+      <div className="ml-3 font-medium">
+        <p className="text-lg font-bold ">{items?.title}</p>
+        <p className="text-xs">{items?.desc}</p>
       </div>
     </div>
   );
