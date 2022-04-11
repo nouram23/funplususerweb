98f154dc1892ca2a0ee2bb898132cd9da236a648
erkaerka1024 <erkaerka1024@gmail.com>
2022-04-11 23:11:03 +0800

    DAY3

--- src/pages/service/index.js
+++ src/pages/service/index.js
@@ -12,112 +12,130 @@ export default function Service() {
       <div>
         <div className="md:mt-24 mt-20 bg-[#1a1a1a] ">
           <div className="mx-auto max-w-screen-2xl">
-            <div className="w-full h-full ">
-              <div className=" bg-[#1f1f1f] flex  pl-10 py-10">
-                <div className="sm:max-w-lg mx-auto ">
-                  <h1 className=" lg:text-2xl text-base mb-4 ">
-                    Үйлчилгээний байгууллагууд
-                  </h1>
-                  <ul className="lg:text-xl md:text-base text-sm font-light space-y-2">
-                    <li>
-                      <a href="#">Barber</a>
-                    </li>
-                    <li>
-                      <a href="#">Baketball</a>
-                    </li>
-                    <li>
-                      <a href="#">Basketball</a>
-                    </li>
-                    <li>
-                      <a href="#">Beauty</a>
-                    </li>
-                    <li>
-                      <a href="#">Billiard</a>
-                    </li>
-                    <li>
-                      <a href="#">Bowling</a>
-                    </li>
-                    <li>
-                      <a href="#">Coach</a>
-                    </li>
-                    <li>
-                      <a href="#">Extereme</a>
-                    </li>
-                    <li>
-                      <a href="#">Karaoke</a>
-                    </li>
-                    <li>
-                      <a href="#">Manicure</a>
-                    </li>
-                    <li>
-                      <a href="#">Maze</a>
-                    </li>
-                    <li>
-                      <a href="#">Paintball</a>
-                    </li>
-                    <li>
-                      <a href="#">Photography</a>
-                    </li>
-                    <li>
-                      <a href="#">PingPong</a>
-                    </li>
-                    <li>
-                      <a href="#">Restourant</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="bg-[#1a1a1a]  flex justify-center mx-auto items-center">
-                <div className="flex justify-center flex-wrap  md:py-10 py-10 sm:px-10 px-2 mx-auto">
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/1.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/2.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/3.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/4.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/5.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/1.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/1.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                  <Item
-                    item={{
-                      icon: "assets/images/billiard-photo/1.png",
-                      name: "Monjoy Billiard Club",
-                    }}
-                  />
-                </div>
+            <div className=" flex  sm:p-10 p-4">
+              <ul className=" md:text-base text-sm font-light flex mx-auto justify-center flex-wrap md:px-10 sm:px-6 px-3 sm:pt-0 pt-4">
+                <List
+                  list={{
+                    name: "Barber",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Basketball",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Beauty",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Billiard",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Bowling",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Coach",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Extereme",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Karaoke",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Manicure",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Maze",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Paintball",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Photography",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "PingPong",
+                  }}
+                />
+                <List
+                  list={{
+                    name: "Restourant",
+                  }}
+                />
+              </ul>
+            </div>
+            <div className="bg-[#1a1a1a]  flex justify-center mx-auto items-center">
+              <div className="flex justify-center flex-wrap pb-10  sm:px-10 px-2 mx-auto">
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/1.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/2.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/3.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/4.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/5.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/1.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/1.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
+                <Item
+                  item={{
+                    icon: "assets/images/billiard-photo/1.png",
+                    name: "Monjoy Billiard Club",
+                  }}
+                />
               </div>
             </div>
           </div>
@@ -128,3 +146,16 @@ export default function Service() {
     </>
   );
 }
+
+const List = ({ list }) => {
+  return (
+    <li className="mx-2 my-2">
+      <button
+        className="py-1 sm:px-4 px-2 rounded-full ring-1 ring-[#9d32c2] hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34]"
+        href="#"
+      >
+        {list?.name}
+      </button>
+    </li>
+  );
+};
