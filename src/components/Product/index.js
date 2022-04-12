a99664f2b2ed119ea5d0381e32846661b9b513ac
erkaerka1024 <erkaerka1024@gmail.com>
2022-04-12 22:17:21 +0800

    day4

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -1,27 +1,19 @@
-export default function Product({ items, longText }) {
-  if (longText) {
-    return (
-      <div className="flex mx-2 my-4 col-span-2">
-        <div className="h-20 p-4 bg-[#ffffff]/80 rounded-2xl mr-3">
-          {" "}
-          <img className="h-14 w-16" src={longText?.url} alt="bg" />
-        </div>
-        <div className=" font-medium">
-          <p className="text-base font-bold ">{longText?.title}</p>
-          <p className="text-xs">{longText?.desc}</p>
-        </div>
-      </div>
-    );
-  }
+export default function Product({ items }) {
   return (
-    <div className="flex mx-2 my-4 col-span-2">
-      <div className="h-20 p-4 bg-[#ffffff]/80 rounded-2xl">
+    <div className="flex  items-center sm:mx-2 my-4 xl:col-span-2 col-span-3">
+      <div className="2xl:h-20 2xl:w-20 xl:h-16 xl:w-16 sm:h-20 sm:w-20 2xl:p-4  xl:p-3 lg:p-4 p-3 bg-[#ffffff]/80 rounded-2xl">
         {" "}
-        <img className="h-14 w-14" src={items?.url} alt="bg" />
+        <img
+          className="2xl:h-14 xl:h-10 sm:h-14   "
+          src={items?.url}
+          alt="bg "
+        />
       </div>
-      <div className="ml-3 font-medium">
-        <p className="text-lg font-bold ">{items?.title}</p>
-        <p className="text-xs">{items?.desc}</p>
+      <div className="s:ml-3 ml-2 font-medium">
+        <p className="sm:text-base font-bold text-xs  sm:leading-5 leading-4">
+          {items?.desc}
+        </p>
+        <p className="text-[10px] 2xl:my-1"> {items?.title}</p>
       </div>
     </div>
   );
--- src/pages/service/booking.js
+++ src/pages/service/booking.js
@@ -12,35 +12,35 @@ const MenuItemGroup = Menu.ItemGroup;
 export default function Service() {
   return (
     <Layout>
-      <div className="md:mt-24 mt-20 bg-[#1a1a1a] ">
-        <div className="max-w-screen-2xl mx-auto ">
+      <div className="mt-24 bg-[#1a1a1a] ">
+        <div className="max-w-screen-2xl  mx-auto lg:px-10 px-2">
           <div className="pl-40 flex h-12 mx-auto font-bold items-center ">
             Нүүр Хуудас-Бүтээгдэхүүн-Billiard
           </div>
-          <div className="flex">
-            <div className="w-3/5">
+          <div className="flex lg:flex-row flex-col lg:justify-center lg:items-start items-center">
+            <div className="lg:w-3/5 md:w-4/5 w-11/12">
               <div>
-                <Carousel className="w-full pr-10" autoplay>
+                <Carousel className="w-full lg:pr-10" autoplay>
                   <img
-                    className="h-[400px] w-[700px] rounded-xl"
+                    className="sm:h-[400px]  h-72  rounded-xl"
                     src="/assets/images/slider/image/1.png"
                     alt="The Woods"
                   />
 
                   <img
-                    className="h-[400px] w-[700px] rounded-xl"
+                    className="sm:h-[400px]  h-72 rounded-xl"
                     src="/assets/images/slider/image/2.png"
                     alt="Cinque Terre"
                   />
 
                   <img
-                    className="h-[400px] w-[700px] rounded-xl"
+                    className="sm:h-[400px]  h-72 rounded-xl"
                     src="/assets/images/slider/image/4.png"
                     alt="Mountains and fjords"
                   />
                 </Carousel>
               </div>
-              <div className="my-9 grid grid-cols-6">
+              <div className="my-9 sm:grid  grid-cols-6 lg:pr-10">
                 <Product
                   items={{
                     url: "/assets/images/billiard-photo/a1.svg",
@@ -63,9 +63,9 @@ export default function Service() {
                   }}
                 />
                 <Product
-                  longText={{
+                  items={{
                     url: "/assets/images/billiard-photo/a5.svg",
-                    title: "Цаг цуцлах боломжтой эсэх",
+                    title: "Цаг цуцлах эсэх",
                     desc: "Цуцлах боломжгүй",
                   }}
                 />
@@ -78,36 +78,26 @@ export default function Service() {
                   }}
                 />
               </div>
-              <div className="mb-9">
-                <Tabs>
-                  <TabPane tab="  Мэдээлэл" key="1">
-                    Дэлхийн стандартын ширээ, хэрэгсэл, тав тухтай орчинд монгол
-                    залуус чөлөөт цагаа зөв боловсон өнгрүүлэхээр боллоо.
-                    Агааржуулалтын систем, цэмбэ, плаж, палк, сандал
-                  </TabPane>
-                  <TabPane tab="Танилцуулга" key="2">
-                    Дэлхийн стандартын ширээ, хэрэгсэл, тав тухтай орчинд монгол
-                    залуус чөлөөт цагаа зөв боловсон өнгрүүлэхээр боллоо.
-                    Агааржуулалтын систем, цэмбэ, плаж, палк, сандал
-                  </TabPane>
-                  <TabPane tab="Бусад" key="3">
-                    Дэлхийн стандартын ширээ, хэрэгсэл, тав тухтай орчинд монгол
-                    залуус чөлөөт цагаа зөв боловсон өнгрүүлэхээр боллоо.
-                    Агааржуулалтын систем, цэмбэ, плаж, палк, сандал{" "}
-                  </TabPane>
-                </Tabs>
+              <div className="mb-9 lg:pr-10">
+                <h1 className="xk:text-2xl text-xl font-bold"> Танилцуулга</h1>
+                <p className=" mt-4 xl:text-lg text-base ">
+                  {" "}
+                  Дэлхийн стандартын ширээ, хэрэгсэл, тав тухтай орчинд монгол
+                  залуус чөлөөт цагаа зөв боловсон өнгрүүлэхээр боллоо.
+                  Агааржуулалтын систем, цэмбэ, плаж, палк, сандал
+                </p>
               </div>
             </div>
-            <div className="w-2/5 space-y-4">
-              <h1 className="text-3xl font-semibold">
+            <div className="lg:w-2/5 md::w-4/5 w-11/12 space-y-4">
+              <h1 className="xl:text-3xl text-lg font-semibold">
                 RED STAR pool Billiard&Karaoke
               </h1>
-              <p className="text-base">
+              <p className="xl:text-base lg:text-sm text-base">
                 Дэлхийн стандартын ширээ, хэрэгсэл, тав тухтай орчинд монгол
                 залуус чөлөөт цагаа зөв боловсон өнгрүүлэхээр боллоо.
                 Агааржуулалтын систем, цэмбэ, плаж, палк, сандал
               </p>
-              <div className="flex justify-center">
+              <div className="flex">
                 <div className="max-w-sm">
                   <Calendar
                     fullscreen={false}
@@ -138,8 +128,8 @@ export default function Service() {
                       const month = value.month();
 
                       return (
-                        <div style={{ padding: 8 }}>
-                          <Row gutter={8}>
+                        <div className="flex justify-end p-2">
+                          <Row gutter={16}>
                             <Col>
                               <Select
                                 size="small"
@@ -160,155 +150,6 @@ export default function Service() {
                     }}
                   />
                 </div>
-              </div>
-              <div className="my-6">
-                <Menu
-                  style={{ width: 700 }}
-                  defaultSelectedKeys={["1"]}
-                  mode="inline"
-                >
-                  <SubMenu title="Table 1">
-                    <MenuItemGroup>
-                      <Row>
-                        <Col span={6}>
-                          <Menu.Item key="1">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="2">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="3">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="4">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              14:30-15:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                      </Row>
-                    </MenuItemGroup>
-                    <MenuItemGroup>
-                      <Row>
-                        <Col span={6}>
-                          <Menu.Item key="5">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              17:30-18:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="6">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="7">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="8">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              18:30-19:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                      </Row>
-                    </MenuItemGroup>
-                  </SubMenu>
-                </Menu>
-              </div>
-              {/* Second Table */}
-              <div className="my-7">
-                <Menu
-                  style={{ width: 700 }}
-                  defaultSelectedKeys={["1"]}
-                  mode="inline"
-                >
-                  <SubMenu title="VIP room Table 1">
-                    <MenuItemGroup>
-                      <Row>
-                        <Col span={6}>
-                          <Menu.Item key="1">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="2">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="3">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="4">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              14:30-15:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                      </Row>
-                    </MenuItemGroup>
-                    <MenuItemGroup>
-                      <Row>
-                        <Col span={6}>
-                          <Menu.Item key="5">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              17:30-18:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="6">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="7">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              12:30-13:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                        <Col span={6}>
-                          <Menu.Item key="8">
-                            <Button className="border-none hover:bg-gradient-to-r from-[#9d32c2] to-[#e97a34] hover:text-white rounded-full text-black font-semibold bg-slate-50 h-10 w-32">
-                              18:30-19:30
-                            </Button>
-                          </Menu.Item>
-                        </Col>
-                      </Row>
-                    </MenuItemGroup>
-                  </SubMenu>
-                </Menu>
               </div>
             </div>
           </div>
