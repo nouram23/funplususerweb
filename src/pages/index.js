f9fecf3e84e67548b0f80987507e2079a16e0f29
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-19 21:06:21 +0800

    addedAPI

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,30 +2,38 @@ import React, { useState, useEffect } from "react";
 import ServiceItem from "components/ServiveItem/item";
 import Layout from "../components/Layout";
 import { Skeleton } from "antd";
+import { data } from "autoprefixer";
 // import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    const timing = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(async () => {
+    const response = await fetch("/api/allServiceType");
+    const types = await response.json();
+    setData(types);
+    console.log(types);
+    setLoading(false);
   }, []);
   return (
     <Layout>
       <div className="md:mt-24 mt-14  bg-[#1a1a1a] ">
         <div className="md:max-w-7xl  max-w-xl mx-auto py-10">
           <div className="flex justify-center flex-wrap mx-auto ">
-            <ServiceItem
-              item={{
-                loading: loading,
-                icon: "assets/images/product/medium-size/1-1.svg",
-                count: 10,
-                name: "Биллиард",
-              }}
-            />
-            <ServiceItem
+            {data.map((e, i) => {
+              return (
+                <ServiceItem
+                  key={i}
+                  item={{
+                    loading: loading,
+                    icon: e.icon,
+                    count: e.count,
+                    name: e.name,
+                  }}
+                />
+              );
+            })}
+            {/* <ServiceItem
               item={{
                 loading: loading,
                 icon: "assets/images/product/medium-size/1-2.svg",
@@ -136,7 +144,7 @@ export default function Home() {
                 count: 10,
                 name: "Ресторан",
               }}
-            />
+            /> */}
             <ServiceItem isComingSoon={true} />
           </div>
         </div>
