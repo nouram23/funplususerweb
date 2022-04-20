3abc68df8b3d7bb57e4be609ac4b37fe8a2269cd
orgil <orgil_ts@outlook.com>
2022-04-21 01:36:15 +0800

    sync

--- src/backend/middlewares/apis/feedback.js
+++ src/backend/middlewares/apis/feedback.js
@@ -1,5 +1,14 @@
-export default function (req, res) {
-  return () => {
-    return [];
+import { apiRequest } from "utils/request";
+
+
+export default function all_service_types(req, res) {
+  return async () => {
+    if (req.method === "POST") {
+      console.log("req.api_urls", req.api_urls)
+      const json = await apiRequest.post(`${req.api_urls.public}/feedback`, data);
+.
+      return {};
+    }
+    throw new Error("API_NOT_FOUND");
   };
 }
