b64a9f03bb8d7df412dea9141ca46ee2768147c9
orgil <orgil_ts@outlook.com>
2022-04-21 01:35:16 +0800

    sync

--- src/backend/middlewares/apis/all_service_types.js
+++ src/backend/middlewares/apis/all_service_types.js
@@ -1,96 +1,14 @@
-export default function (req, res) {
-  return () => {
-    return [
-      {
-        icon: "assets/images/product/medium-size/1-1.svg",
-        count: 10,
-        name: "Биллиард",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-2.svg",
-        count: 10,
-        name: "Үсчин",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-3.svg",
-        count: 10,
-        name: "Заал",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-4.svg",
-        count: 10,
-        name: "Усан бассейн",
-        type: "special",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-5.svg",
-        count: 10,
-        name: "Гоо сайхан",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-6.svg",
-        count: 10,
-        name: "Боулинг",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-7.svg",
-        count: 10,
-        name: "Сэтгэл судлаач",
-        type: "special",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-8.svg",
-        count: 10,
-        name: "Экстрем спорт",
-        type: "special",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-9.svg",
-        count: 10,
-        name: "Кароеке",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-10.svg",
-        count: 10,
-        name: "Маникюр",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-11.svg",
-        count: 10,
-        name: "Энтертаймент төв",
-        type: "special",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-12.svg",
-        count: 10,
-        name: "Пейнтбол",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-13.svg",
-        count: 10,
-        name: "Зургийн студи",
-        type: "special",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-14.svg",
-        count: 10,
-        name: "Теннис",
-        type: "simple",
-      },
-      {
-        icon: "assets/images/product/medium-size/1-15.svg",
-        count: 10,
-        name: "Ресторан",
-        type: "simple",
-      },
-    ];
+import { apiRequest } from "utils/request";
+
+
+export default function all_service_types(req, res) {
+  return async () => {
+    if (req.method === "GET") {
+      console.log("req.api_urls", req.api_urls)
+      const json = await apiRequest.get(`${req.api_urls.public}/service_types`, data);
+
+      return json;
+    }
+    throw new Error("API_NOT_FOUND");
   };
 }
