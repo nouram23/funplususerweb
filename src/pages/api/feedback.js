5b9c03ba76e44ea2b554e6c10d64164d0fb443bb
З. Эрдэнэ-Уул <zerdeneuul2001@gmail.com>
2022-04-20 19:27:12 +0800

    feedback

---
+++ src/pages/api/feedback.js
@@ -0,0 +1,20 @@
+import feedback from "backend/middlewares/apis/feedback";
+
+const infos = feedback()();
+
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    res.status(200).json(infos);
+  } else if (req.method === "POST") {
+    const info = req.body.values;
+    const newInfo = {
+      companyName: info.companyName,
+      phone: info.phone,
+      email: info.email,
+      workerName: info.workerName,
+      serviceTypes: info.serviceTypes,
+    };
+    infos.push(newInfo);
+    res.status(201).json(newInfo);
+  }
+}
